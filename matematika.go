be9b8827b49d0b109dc65c15838ee366606a7981
AkbarZul <akbar.zul73@gmail.com>
2021-11-18 13:26:02 +0700

    operasi matematika

---
+++ matematika.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var result = 10 + 10
+	fmt.Println(result)
+
+	var a = 10
+	var b = 10
+	var c = a * b
+	fmt.Println(c)
+
+	var i = 10
+	i += 10 // i = 1 + 10
+	fmt.Println(i)
+	i++ // i = 1 + 1
+	fmt.Println(i)
+}
